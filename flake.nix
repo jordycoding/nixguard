a5ad2e64e00a100f17ec174432854b8ecd5fbb2d
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2024-03-30 23:45:58 +0100

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A wireguard configuration manager for nixos";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+    ]
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          version = builtins.substring 0 8 self.lastModifiedDate;
+        in
+        {
+          packages = {
+            default = pkgs.rustPlatform.buildRustPackage {
+              pname = "nixguard";
+              inherit version;
+              src = ./.;
+
+              cargoHash = "";
+            };
+          };
+
+          apps.default = utils.lib.mkApp { drv = self.packages.${system}.default; };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+          };
+        });
+}
+
